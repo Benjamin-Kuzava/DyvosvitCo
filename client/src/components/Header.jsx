d2c6731e6c1bc87d7b4feb89385c3a0eee819244
Benjamin-Kuzava <kuzav1bj@gmail.com>
2021-05-04 17:20:35 -0400

    Continue work on header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -16,6 +16,38 @@ export default function Header() {
     history.push(ROUTES.SIGN_IN);
   };
 
+  const unauthenticatedOptions = (
+    <>
+      <NavLink to={ROUTES.SIGN_IN}>
+        <button
+          type="button"
+          className="font-bold text-base text-black-base font-Itim rounded w-20 h-8"
+        >
+          Sign In
+        </button>
+      </NavLink>
+      <NavLink to={ROUTES.SIGN_UP}>
+        <button
+          type="button"
+          className="font-bold text-base text-white bg-gray-base font-Itim rounded w-20 h-8"
+        >
+          Sign Up
+        </button>
+      </NavLink>
+    </>
+  );
+
+  const authenticatedOptions = (
+    <NavLink to={ROUTES.SIGN_IN}>
+      <button
+        type="button"
+        className="font-bold text-sm text-black-base font-Itim rounded w-20 h-8"
+      >
+        Profile
+      </button>
+    </NavLink>
+  );
+
   return (
     <header className="h-16 mb-8 bg-white border-b border-gray-primary">
       <div className="container mx-auto max-w-screen-lg h-full">
@@ -27,33 +59,16 @@ export default function Header() {
               </NavLink>
             </h1>
           </div>
-
-          <div className="flex justify-center items-center text-gray-base cursor-pointer">
-            {user ? (
-              <>
-                <p>user</p>
-              </>
-            ) : (
-              <>
-                <p>no user</p>
-              </>
-            )}
-          </div>
-
           <div className="flex justify-center items-center text-black-base cursor-pointer">
             <button type="button" className="w-20 h-8 flex justify-center items-center">
               <SearchIcon />
             </button>
-            |
-            <NavLink to={ROUTES.SIGN_IN}>
-              <button
-                type="button"
-                className="font-bold text-sm text-black-base font-Itim rounded w-20 h-8"
-              >
-                Sign In
-              </button>
-            </NavLink>
-            <NavLink to={ROUTES.CART} aria-label="View Cart">
+            |{user ? authenticatedOptions : unauthenticatedOptions}
+            <NavLink
+              to={ROUTES.CART}
+              aria-label="View Cart"
+              className="w-20 flex justify-center items-center"
+            >
               <CartIcon />
             </NavLink>
           </div>
